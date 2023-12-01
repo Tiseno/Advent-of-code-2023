a738156583b2660015b7bb944f17072d28eb8d7a
Tiseno <mathias.lindgren@stabelo.se>
2023-12-01 07:14:05 +0100

    Day 1 refactor.

--- day01/main.hs
+++ day01/main.hs
@@ -1,58 +1,25 @@
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-
-{-# HLINT ignore "Use <$>" #-}
-{-# HLINT ignore "Functor law" #-}
-{-# HLINT ignore "Eta reduce" #-}
 import qualified Data.Char as Char
 
-parseInput input = lines input
+wordDigits :: String -> String
+wordDigits xs@('o':'n':'e':_) = '1' : wordDigits (tail xs)
+wordDigits xs@('t':'w':'o':_) = '2' : wordDigits (tail xs)
+wordDigits xs@('t':'h':'r':'e':'e':_) = '3' : wordDigits (tail xs)
+wordDigits xs@('f':'o':'u':'r':_) = '4' : wordDigits (tail xs)
+wordDigits xs@('f':'i':'v':'e':_) = '5' : wordDigits (tail xs)
+wordDigits xs@('s':'i':'x':_) = '6' : wordDigits (tail xs)
+wordDigits xs@('s':'e':'v':'e':'n':_) = '7' : wordDigits (tail xs)
+wordDigits xs@('e':'i':'g':'h':'t':_) = '8' : wordDigits (tail xs)
+wordDigits xs@('n':'i':'n':'e':_) = '9' : wordDigits (tail xs)
+wordDigits (x:xs)
+  | Char.isDigit x = x : wordDigits xs
+wordDigits (_:xs) = wordDigits xs
+wordDigits [] = []
 
-part1 input = sum $ fmap f $ fmap (filter Char.isDigit) input
-  where
-    f l = read [head l, last l]
-
-toOnlyDigits :: String -> String
-toOnlyDigits ('o':'n':'e':xs) = '1' : toOnlyDigits xs
-toOnlyDigits ('t':'w':'o':xs) = '2' : toOnlyDigits xs
-toOnlyDigits ('t':'h':'r':'e':'e':xs) = '3' : toOnlyDigits xs
-toOnlyDigits ('f':'o':'u':'r':xs) = '4' : toOnlyDigits xs
-toOnlyDigits ('f':'i':'v':'e':xs) = '5' : toOnlyDigits xs
-toOnlyDigits ('s':'i':'x':xs) = '6' : toOnlyDigits xs
-toOnlyDigits ('s':'e':'v':'e':'n':xs) = '7' : toOnlyDigits xs
-toOnlyDigits ('e':'i':'g':'h':'t':xs) = '8' : toOnlyDigits xs
-toOnlyDigits ('n':'i':'n':'e':xs) = '9' : toOnlyDigits xs
-toOnlyDigits (x:xs)
-  | Char.isDigit x = x : toOnlyDigits xs
-toOnlyDigits (_:xs) = toOnlyDigits xs
-toOnlyDigits [] = []
-
-toOnlyDigitsRR :: String -> String
-toOnlyDigitsRR input = reverse $ toOnlyDigitsR $ reverse input
-  where
-    toOnlyDigitsR :: String -> String
-    toOnlyDigitsR ('e':'n':'o':xs) = '1' : toOnlyDigitsR xs
-    toOnlyDigitsR ('o':'w':'t':xs) = '2' : toOnlyDigitsR xs
-    toOnlyDigitsR ('e':'e':'r':'h':'t':xs) = '3' : toOnlyDigitsR xs
-    toOnlyDigitsR ('r':'u':'o':'f':xs) = '4' : toOnlyDigitsR xs
-    toOnlyDigitsR ('e':'v':'i':'f':xs) = '5' : toOnlyDigitsR xs
-    toOnlyDigitsR ('x':'i':'s':xs) = '6' : toOnlyDigitsR xs
-    toOnlyDigitsR ('n':'e':'v':'e':'s':xs) = '7' : toOnlyDigitsR xs
-    toOnlyDigitsR ('t':'h':'g':'i':'e':xs) = '8' : toOnlyDigitsR xs
-    toOnlyDigitsR ('e':'n':'i':'n':xs) = '9' : toOnlyDigitsR xs
-    toOnlyDigitsR (x:xs)
-      | Char.isDigit x = x : toOnlyDigitsR xs
-    toOnlyDigitsR (_:xs) = toOnlyDigitsR xs
-    toOnlyDigitsR [] = []
-
-part2 :: [String] -> Int
-part2 input = sum $ fmap f input
-  where
-    f l = read [head (toOnlyDigits l), last (toOnlyDigitsRR l)] :: Int
+solution f input = sum $ (\l -> read [head l, last l]) . f <$> lines input
 
 main = do
   input <- readFile "input.txt"
-  let parsed = parseInput input
   putStrLn "Part 1"
-  print $ part1 parsed
+  print $ solution (filter Char.isDigit) input
   putStrLn "Part 2"
-  print $ part2 parsed
+  print $ solution wordDigits input
