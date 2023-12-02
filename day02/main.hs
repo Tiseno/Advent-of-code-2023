b07a1650b0accebf33737623dbcdca2d23708a0d
Tiseno <mathias.lindgren@stabelo.se>
2023-12-02 09:17:16 +0100

    Day 2 part 2.

--- day02/main.hs
+++ day02/main.hs
@@ -1,9 +1,6 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
 {-# HLINT ignore "Use <$>" #-}
-{-# HLINT ignore "Functor law" #-}
-{-# HLINT ignore "Eta reduce" #-}
-{-# HLINT ignore "Redundant id" #-}
 import qualified Data.Bifunctor  as Bifunctor
 import qualified Data.Char       as Char
 import qualified Data.List.Split as Split
@@ -27,8 +24,7 @@ parseInput input = fmap parseLine $ lines input
       let [gs, line'] = Split.splitOn ": " line
           i = read $ drop 5 gs
        in Game i $
-          fmap (fmap parseCube) $
-          fmap (Split.splitOn ", ") $ Split.splitOn "; " line'
+          fmap (fmap parseCube . Split.splitOn ", ") $ Split.splitOn "; " line'
     parseCube s =
       let (d, s') = Bifunctor.first read $ span Char.isDigit s
        in (case drop 1 s' of
@@ -38,15 +34,25 @@ parseInput input = fmap parseLine $ lines input
             d
 
 part1 (redLimit, greenLimit, blueLimit) (input :: [Game]) =
-  sum $ fmap gameId $ filter f input
+  sum $ fmap gameId $ filter possibleGame input
   where
-    f Game {gameRounds} = all (all cubePossible) gameRounds
+    possibleGame Game {gameRounds} = all cubePossible $ concat gameRounds
     cubePossible (Red i)   = i <= redLimit
     cubePossible (Green i) = i <= greenLimit
     cubePossible (Blue i)  = i <= blueLimit
+
+part2 (input :: [Game]) = sum $ fmap (power . findMax) input
+  where
+    power (r, g, b) = r * g * b
+    findMax Game {gameRounds} = foldl maxFold (0, 0, 0) $ concat gameRounds
+    maxFold (r, g, b) (Red i)   = (max r i, g, b)
+    maxFold (r, g, b) (Green i) = (r, max g i, b)
+    maxFold (r, g, b) (Blue i)  = (r, g, max b i)
 
 main = do
   input <- readFile "input.txt"
   let games = parseInput input
   putStrLn "Part 1"
   print $ part1 (12, 13, 14) games
+  putStrLn "Part 2"
+  print $ part2 games
