b8ff3930944fd7a3a23a89d1ba4eb515ec8c5895
Tiseno <mathias.lindgren@stabelo.se>
2023-12-04 06:48:56 +0100

    Day 4 part 2.

--- day04/main.hs
+++ day04/main.hs
@@ -1,6 +1,9 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use <$>" #-}
 import qualified Data.List.Split as Split
+import qualified Data.MultiSet   as MultiSet
 import qualified Data.Set        as Set
-import qualified Debug.Trace     as Debug
 
 data Card =
   Card
@@ -28,8 +31,24 @@ part1 input = sum $ fmap scoreOfCard input
             then 0
             else 2 ^ (n - 1)
 
+part2 input =
+  sum $
+  fmap snd $
+  MultiSet.toOccurList $
+  foldl scoreOfCard (MultiSet.fromList [1 .. (length input)]) input
+  where
+    scoreOfCard cardCount Card {cardId, winningNumbers, ourNumbers} =
+      let wins = length $ winningNumbers `Set.intersection` ourNumbers
+          cardIds = [(cardId + 1) .. (cardId + wins)]
+       in countWins (MultiSet.occur cardId cardCount) cardCount cardIds
+    countWins multiplier cardCount [] = cardCount
+    countWins multiplier cardCount (x:xs) =
+      countWins multiplier (MultiSet.insertMany x multiplier cardCount) xs
+
 main = do
   input <- readFile "input.txt"
   let parsed = parseInput input
   putStrLn "Part 1"
   print $ part1 parsed
+  putStrLn "Part 2"
+  print $ part2 parsed
