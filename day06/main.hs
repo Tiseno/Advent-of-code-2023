343ced4ed759b369b62ccd14e3fd2c28bc8936a0
Tiseno <mathias.lindgren@stabelo.se>
2023-12-06 07:02:30 +0100

    Day 6 part 2.

--- day06/main.hs
+++ day06/main.hs
@@ -1,7 +1,7 @@
 import qualified Data.List.Split as Split
 
-parseInput :: String -> [(Int, Int)]
-parseInput input =
+parseInput1 :: String -> [(Int, Int)]
+parseInput1 input =
   let [[_, times], [_, distances]] = Split.splitOn ":" <$> lines input
    in zip (read <$> words times) (read <$> words distances)
 
@@ -11,8 +11,28 @@ part1 races = product $ fmap (length . waysToBeat) races
     waysToBeat (time, dist) =
       filter (> dist) $ (\x -> x * (time - x)) <$> [0 .. time]
 
+parseInput2 :: String -> (Int, Int)
+parseInput2 input =
+  let [[_, times], [_, distances]] = Split.splitOn ":" <$> lines input
+   in (read $ concat $ words times, read $ concat $ words distances)
+
+-- Better version, find the smallest x and the biggest x with binary search
+part2 (time, dist) = 1 + findBound time time (-1) - findBound 0 time 1
+  where
+    raceFormula x = x * (time - x)
+    findBound x p d =
+      let p' = max 1 (p `div` 2)
+       in if raceFormula x < dist && raceFormula (x + d) >= dist
+            then x + d
+            else if raceFormula x < dist
+                   then findBound (x + (d * p')) p' d
+                   else findBound (x - (d * p')) p' d
+
 main = do
   input <- readFile "input.txt"
-  let parsed = parseInput input
+  let parsed = parseInput1 input
   putStrLn "Part 1"
   print $ part1 parsed
+  let parsed = parseInput2 input
+  putStrLn "Part 2"
+  print $ part2 parsed
