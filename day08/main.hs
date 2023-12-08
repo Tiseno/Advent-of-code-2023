71f6ccd61ee1f308e0d8810f210eb282408621b9
Tiseno <mathias.lindgren@stabelo.se>
2023-12-08 07:12:36 +0100

    Day 8 naive defective part 2.

--- day08/main.hs
+++ day08/main.hs
@@ -5,6 +5,8 @@ newtype Node =
   Node (Char, Char, Char)
   deriving (Eq, Ord)
 
+lastChar (Node (_, _, c)) = c
+
 instance Show Node where
   show (Node (a, b, c)) = [a, b, c]
 
@@ -21,19 +23,33 @@ parseInput input =
       let [src, destinations] = Split.splitOn " = " l
        in (read src, read $ filter (/= ' ') destinations)
 
-part1 (steps, nodes) = walk 0 (cycle steps) (read "AAA") (read "ZZZ")
+choose 'L' (l, _) = l
+choose 'R' (_, r) = r
+
+part1 (steps, nodes) = walk (cycle steps) 0 startingNode
   where
     nodeMap = Map.fromList nodes
-    walk count _ current target
-      | current == target = count
-    walk count (x:steps) current target =
-      let (l, r) = nodeMap Map.! current
-       in case x of
-            'L' -> walk (count + 1) steps l target
-            'R' -> walk (count + 1) steps r target
+    startingNode = read "AAA"
+    targetNode = read "ZZZ"
+    walk _ count current
+      | current == targetNode = count
+    walk (x:steps) count current =
+      walk steps (count + 1) (choose x $ nodeMap Map.! current)
+
+part2 (steps, nodes) = walk (cycle steps) 0 startingNodes
+  where
+    nodeMap = Map.fromList nodes
+    startingNodes = filter (('A' ==) . lastChar) $ fmap fst nodes
+    targetChar = 'Z'
+    walk _ count currents
+      | all ((targetChar ==) . lastChar) currents = count
+    walk (x:steps) count currents =
+      walk steps (count + 1) $ choose x <$> fmap (nodeMap Map.!) currents
 
 main = do
   input <- readFile "input.txt"
   let parsed = parseInput input
   putStrLn "Part 1"
   print $ part1 parsed
+  putStrLn "Part 2"
+  print $ part2 parsed
