ce3a47d0fec2de31c2fa04b6a10161268d93b5f1
Tiseno <mathias.lindgren@stabelo.se>
2023-12-09 06:38:49 +0100

    Day 9 part 2.

--- day09/main.hs
+++ day09/main.hs
@@ -1,17 +1,22 @@
 parseInput input = fmap read . words <$> lines input
 
-part1 (histories :: [[Int]]) = sum $ fmap extrapolateNext histories
-  where
-    extrapolateNext (history :: [Int]) = extrapolate $ allDerivatives history
-    allDerivatives history
-      | any (0 /=) history =
-        let d = zipWith (-) (tail history) history
-         in history : allDerivatives d
-    allDerivatives history = [history]
-    extrapolate histories = sum $ fmap last histories
+allDerivatives history
+  | any (0 /=) history =
+    let d = zipWith (-) (tail history) history
+     in history : allDerivatives d
+allDerivatives history = [history]
+
+solve (histories :: [[Int]]) extrapolationFn =
+  sum $ fmap (extrapolationFn . allDerivatives) histories
+
+extrapolateNext histories = sum $ fmap last histories
+
+extrapolatePrev histories = foldr (-) 0 $ fmap head histories
 
 main = do
   input <- readFile "input.txt"
   let parsed = parseInput input
   putStrLn "Part 1"
-  print $ part1 parsed
+  print $ solve parsed extrapolateNext
+  putStrLn "Part 2"
+  print $ solve parsed extrapolatePrev
