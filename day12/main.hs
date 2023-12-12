4acd5ec0881747aee6af55904a9fbae17d65bad2
Tiseno <mathias.lindgren@stabelo.se>
2023-12-12 09:10:40 +0100

    Day 12 minor refactor.

--- day12/main.hs
+++ day12/main.hs
@@ -9,40 +9,39 @@ parseRecords input = parseRecord <$> lines input
       Bifunctor.second (\d -> read <$> Split.splitOn "," d :: [Int]) $
       (\[layout, groups] -> (layout, groups)) $ Split.splitOn " " line
 
-arrangementSum (input :: [(String, [Int])]) = sum $ fst <$> fmap solveLine input
-
-solveLine ((layout, groups) :: (String, [Int])) = search Map.empty layout groups
-
-canBeDamaged c = c == '?' || c == '#'
-
-canBeOperational c = c == '?' || c == '.'
+arrangementSum (input :: [(String, [Int])]) = sum $ fmap arrangements input
 
 type Memo = Map.Map (Int, Int) Int
 
-search :: Memo -> String -> [Int] -> (Int, Memo)
-search m layout []
-  | all canBeOperational layout = (1, m)
-search m _ [] = (0, m)
-search m [] (_:_) = (0, m)
-search m layout@(l:ls) groups@(g:gs) =
-  let key = (length layout, length groups)
-   in case Map.lookup key m of
-        Just i -> (i, m)
-        Nothing ->
-          let nextPossibleDamaged = take g layout
-              afterPossibleDamaged = drop g $ take (g + 1) layout
-              (r1, m') =
-                if length nextPossibleDamaged == g &&
-                   all canBeDamaged nextPossibleDamaged &&
-                   all canBeOperational afterPossibleDamaged
-                  then search m (drop (g + 1) layout) gs
-                  else (0, m)
-              (r2, m'') =
-                if canBeOperational l
-                  then search m' ls groups
-                  else (0, m')
-              r = r1 + r2
-           in (r, Map.insert key r m'')
+arrangements :: (String, [Int]) -> Int
+arrangements (layout, group) = snd $ search' Map.empty layout group
+  where
+    canBeDamaged c = c == '?' || c == '#'
+    canBeOperational c = c == '?' || c == '.'
+    search' :: Memo -> String -> [Int] -> (Memo, Int)
+    search' m layout []
+      | all canBeOperational layout = (m, 1)
+    search' m _ [] = (m, 0)
+    search' m [] (_:_) = (m, 0)
+    search' m layout@(l:ls) groups@(g:gs) =
+      let key = (length layout, length groups)
+       in case Map.lookup key m of
+            Just i -> (m, i)
+            Nothing ->
+              let nextPossibleDamaged = take g layout
+                  afterPossibleDamaged = drop g $ take (g + 1) layout
+                  (m', r1) =
+                    if length nextPossibleDamaged == g &&
+                       all canBeDamaged nextPossibleDamaged &&
+                       all canBeOperational afterPossibleDamaged
+                      then search' m (drop (g + 1) layout) gs
+                      else (m, 0)
+                  (m'', r2) =
+                    if canBeOperational l
+                      then search' m' ls groups
+                      else (m', 0)
+                  r = r1 + r2
+               in (Map.insert key r m'', r)
 
 unfoldRecords (layout, groups) =
   (List.intercalate "?" $ replicate 5 layout, concat $ replicate 5 groups)
