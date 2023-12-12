e9b31b1918ee9864112acb137ec2918fa4dc3170
Tiseno <mathias.lindgren@stabelo.se>
2023-12-12 08:26:50 +0100

    Day 12 part 2.

--- day12/main.hs
+++ day12/main.hs
@@ -1,44 +1,56 @@
 import qualified Data.Bifunctor  as Bifunctor
+import qualified Data.List       as List
 import qualified Data.List.Split as Split
+import qualified Data.Map        as Map
 
-parseInput input = parseLine <$> lines input
+parseRecords input = parseRecord <$> lines input
   where
-    parseLine line =
+    parseRecord line =
       Bifunctor.second (\d -> read <$> Split.splitOn "," d :: [Int]) $
       (\[layout, groups] -> (layout, groups)) $ Split.splitOn " " line
 
-part1 (input :: [(String, [Int])]) = sum $ length <$> fmap solveLine input
+arrangementSum (input :: [(String, [Int])]) = sum $ fst <$> fmap solveLine input
 
-solveLine ((layout, groups) :: (String, [Int])) = search "" layout groups
+solveLine ((layout, groups) :: (String, [Int])) = search Map.empty layout groups
 
 canBeDamaged c = c == '?' || c == '#'
 
 canBeOperational c = c == '?' || c == '.'
 
-isUnknown c = c == '?'
+type Memo = Map.Map (Int, Int) Int
 
-search :: String -> String -> [Int] -> [String]
-search prev layout [] =
-  [prev ++ replicate (length layout) '.' | all canBeOperational layout]
-search prev [] (_:_) = []
-search prev layout@(l:ls) groups@(g:gs) =
-  let nextPossibleDamaged = take g layout
-      afterPossibleDamaged = drop g $ take (g + 1) layout
-   in (if length nextPossibleDamaged == g &&
-          all canBeDamaged nextPossibleDamaged &&
-          all canBeOperational afterPossibleDamaged
-         then search
-                (prev ++
-                 ('#' <$ nextPossibleDamaged) ++ ('.' <$ afterPossibleDamaged))
-                (drop (g + 1) layout)
-                gs
-         else []) ++
-      if canBeOperational l
-        then search (prev ++ ".") ls groups
-        else []
+search :: Memo -> String -> [Int] -> (Int, Memo)
+search m layout []
+  | all canBeOperational layout = (1, m)
+search m _ [] = (0, m)
+search m [] (_:_) = (0, m)
+search m layout@(l:ls) groups@(g:gs) =
+  let key = (length layout, length groups)
+   in case Map.lookup key m of
+        Just i -> (i, m)
+        Nothing ->
+          let nextPossibleDamaged = take g layout
+              afterPossibleDamaged = drop g $ take (g + 1) layout
+              (r1, m') =
+                if length nextPossibleDamaged == g &&
+                   all canBeDamaged nextPossibleDamaged &&
+                   all canBeOperational afterPossibleDamaged
+                  then search m (drop (g + 1) layout) gs
+                  else (0, m)
+              (r2, m'') =
+                if canBeOperational l
+                  then search m' ls groups
+                  else (0, m')
+              r = r1 + r2
+           in (r, Map.insert key r m'')
+
+unfoldRecords (layout, groups) =
+  (List.intercalate "?" $ replicate 5 layout, concat $ replicate 5 groups)
 
 main = do
   input <- readFile "input.txt"
-  let parsed = parseInput input
+  let records = parseRecords input
   putStrLn "Part 1"
-  print $ part1 parsed
+  print $ arrangementSum records
+  putStrLn "\nPart 2"
+  print $ arrangementSum $ fmap unfoldRecords records
