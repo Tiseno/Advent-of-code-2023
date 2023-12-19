e4960b9ba0ebffd1ba2df81129602ce513d22c9b
Tiseno <mathias.lindgren@stabelo.se>
2023-12-19 08:24:15 +0100

    Day 19 part 2.

--- day19/main.hs
+++ day19/main.hs
@@ -11,12 +11,12 @@ data Rule =
 
 type Workflow = (Name, [Rule])
 
-data Part =
+data Part a =
   Part
-    { _x :: Int
-    , _m :: Int
-    , _a :: Int
-    , _s :: Int
+    { _x :: a
+    , _m :: a
+    , _a :: a
+    , _s :: a
     }
   deriving (Show, Eq)
 
@@ -39,7 +39,7 @@ parseInput input =
                      (op:i) = rule
                   in Rule (Just (head targetOrCat, op, read i)) target
 
-part1 (workflows :: [Workflow]) (parts :: [Part]) =
+part1 (workflows :: [Workflow]) (parts :: [Part Int]) =
   foldl (\a (Part x1 m1 a1 s1) -> a + x1 + m1 + a1 + s1) 0 acceptedParts
   where
     acceptedParts = filter (runWorkflows "in") parts
@@ -68,8 +68,59 @@ part1 (workflows :: [Workflow]) (parts :: [Part]) =
             then Just target
             else Nothing
 
+part2 workflows =
+  sum $
+  (\(_, p) -> partCombinations p) <$>
+  filter
+    (\(t, p) -> t == "A")
+    ((!! 7) $
+     iterate runWorkflows [("in", Part (1, 4000) (1, 4000) (1, 4000) (1, 4000))])
+  where
+    partCombinations (Part (x, x') (m, m') (a, a') (s, s')) =
+      (1 + x' - x) * (1 + m' - m) * (1 + a' - a) * (1 + s' - s)
+    runWorkflows (targetAndPart :: [(Name, Part (Int, Int))]) =
+      concatMap fst (runWorkflow <$> targetAndPart)
+    runWorkflow (['A'], part) = ([("A", part)], Nothing)
+    runWorkflow (['R'], part) = ([("R", part)], Nothing)
+    runWorkflow (target, part) =
+      foldl foldRule ([], Just part) (workflowMap Map.! target)
+    workflowMap = Map.fromList workflows
+    foldRule (acc :: [(Name, Part (Int, Int))], Nothing) _ = (acc, Nothing)
+    foldRule (acc, Just part :: Maybe (Part (Int, Int))) (Rule Nothing target) =
+      (acc ++ [(target, part)], Nothing)
+    foldRule (acc, Just part :: Maybe (Part (Int, Int))) (Rule (Just (cat, op, i)) target) =
+      let (matching, notMatching) = slicePartOnCategory part cat op i
+          acc' =
+            case matching of
+              Just m  -> acc ++ [(target, m)]
+              Nothing -> acc
+       in (acc', notMatching)
+    sliceRange (min, max) i '<'
+      | max < i = (Just (min, max), Nothing)
+      | i <= min = (Nothing, Just (min, max))
+      | otherwise = (Just (min, i - 1), Just (i, max))
+    sliceRange (min, max) i '>'
+      | min > i = (Just (min, max), Nothing)
+      | i >= max = (Nothing, Just (min, max))
+      | otherwise = (Just (i + 1, max), Just (min, i))
+    slicePartOnCategory (part :: Part (Int, Int)) catName op i =
+      let (cat, catUpdate) =
+            case catName of
+              'x' -> (_x, \p v -> p {_x = v})
+              'm' -> (_m, \p v -> p {_m = v})
+              'a' -> (_a, \p v -> p {_a = v})
+              's' -> (_s, \p v -> p {_s = v})
+          range = cat part
+          (matchedRange, notMatchedRange) = sliceRange range i op
+          m =
+            ( catUpdate part <$> matchedRange
+            , catUpdate part <$> notMatchedRange)
+       in m
+
 main = do
   input <- readFile "input.txt"
   let (workflows, parts) = parseInput input
   putStrLn "Part 1"
   print $ part1 workflows parts
+  putStrLn "Part 2"
+  print $ part2 workflows
