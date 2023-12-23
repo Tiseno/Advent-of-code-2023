b1880c9e2b4872b2957766d7869a3c5185154045
Tiseno <mathias.lindgren@stabelo.se>
2023-12-23 15:07:39 +0100

    Day 23 part 2.

--- day23/main.hs
+++ day23/main.hs
@@ -1,5 +1,10 @@
 import qualified Data.Array      as Array
 import qualified Data.List.Extra as List
+import qualified Data.Map        as Map
+import qualified Data.Maybe      as Maybe
+import qualified Data.MultiMap   as MultiMap
+import qualified Data.Ord        as Ord
+import qualified Data.Set        as Set
 
 parseInput input =
   Array.array
@@ -38,9 +43,70 @@ part1 m = do
           se <- mapM (\r -> step r (steps + 1, m Array.// [(r, 'O')])) reachable
           pure (concat se)
 
+part2 m = sum $ search Set.empty start [0]
+  where
+    start = (1, 0)
+    bounds = Array.bounds m
+    (xMax, yMax) = snd bounds
+    destination = (xMax - 1, yMax)
+    left (x, y) = (x - 1, y)
+    right (x, y) = (x + 1, y)
+    up (x, y) = (x, y - 1)
+    down (x, y) = (x, y + 1)
+    reachableNeighbors pos m =
+      filter
+        (\p ->
+           Array.inRange bounds p && m Array.! p `List.notElem` ['S', 'O', '#'])
+        [left pos, right pos, up pos, down pos]
+    isNode m p = m Array.! p /= '#' && length (reachableNeighbors p m) > 2
+    nodes =
+      Set.insert destination $
+      Set.fromList $ filter (isNode m) $ Array.indices m
+    walkToNode (m0, edges) src (steps, current) =
+      if Set.member current nodes
+        then let newEdges = (List.sort [src, current], steps + 1) : edges
+              in case reachableNeighbors current m0 of
+                   [] -> (m0, newEdges)
+                   reachable ->
+                     foldl
+                       (\s r -> walkToNode s current (0, r))
+                       (m0, newEdges)
+                       reachable
+        else let m1 = m0 Array.// [(current, 'O')]
+              in case filter (src /=) $ reachableNeighbors current m1 of
+                   []     -> (m1, edges)
+                   [next] -> walkToNode (m1, edges) src (steps + 1, next)
+    withStart = m Array.// [(start, 'S')]
+    distances =
+      Map.fromList $ snd $ walkToNode (withStart, []) start (-1, start)
+    edges =
+      MultiMap.toMap $
+      MultiMap.fromList $
+      concatMap (\([a, b], _) -> [(a, b), (b, a)]) $ Map.toList distances
+    search _ current dist
+      | current == destination = dist
+    search visited current dist =
+      let nexts = Maybe.fromMaybe [] $ Map.lookup current edges
+          unvisitedNexts = filter (`Set.notMember` visited) nexts
+          paths =
+            fmap
+              (\next ->
+                 search
+                   (Set.insert current visited)
+                   next
+                   (dist ++ [distances Map.! List.sort [current, next]]))
+              unvisitedNexts
+       in if null paths
+            then []
+            else snd $
+                 List.maximumBy (Ord.comparing fst) $
+                 (\path -> (sum path, path)) <$> paths
+
 main = do
   input <- readFile "input.txt"
   let parsed = parseInput input
   putStrLn "Part 1"
   r <- part1 parsed
   print r
+  putStrLn "Part 2"
+  print $ part2 parsed
